7614da9aff95f1187d45bdd1812e96604d67f3f2
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-02 13:24:45 +0300

    handling the login

--- Animax_back/src/controllers/users/authControllers.ts
+++ Animax_back/src/controllers/users/authControllers.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { genSalt, hash } from "bcryptjs";
+import bcrypt, { genSalt, hash } from "bcryptjs";
 import User from "../../models/User";
 import jwt from "jsonwebtoken";
 export const register = async (req: Request, res: Response) => {
@@ -20,7 +20,7 @@ export const register = async (req: Request, res: Response) => {
       return;
     }
     const auth = await User.create(securized);
-    const token = signToken(auth);
+    const token = await signToken(auth);
     res.status(200).json({ res: token });
     return;
   } catch (error) {
@@ -29,7 +29,30 @@ export const register = async (req: Request, res: Response) => {
   }
 };
 export const login = async (req: Request, res: Response) => {
-  res.json({ s: "ssss" });
+  const { passwords, email } = req.body;
+  if (!passwords || !email) {
+    res.status(400).json({ res: "email and passwords are required" });
+    return;
+  }
+  // check passwords
+  const user = await User.findOne({ email });
+  if (!user) {
+    res
+      .status(400)
+      .json({ res: "please verify you have enter the correct email" });
+    return;
+  }
+  const correctPsswd = bcrypt.compareSync(passwords, user?.passwords);
+  if (!correctPsswd) {
+    res.status(401).json({ err: "incrorrect passwords" });
+    return;
+  }
+  const token = signToken(user);
+  if (token == "") {
+    res.status(500).json({ err: "an error occured during token's creation" });
+    return;
+  }
+  res.status(200).json({ res: token });
 };
 const signToken = (auth: any): string => {
   const token = jwt.sign(
