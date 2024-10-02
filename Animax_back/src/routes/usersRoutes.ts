1b7d031c34cf7e1bc0bfc7d1a9cbf5c0add225b8
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-02 10:42:08 +0300

    adding the proper routes for register and login

--- Animax_back/src/routes/usersRoutes.ts
+++ Animax_back/src/routes/usersRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { register } from "../controllers/users/authControllers";
+import { login, register } from "../controllers/users/authControllers";
 
 const userRouter: Router = Router();
-userRouter.post("/", register);
-export default userRouter
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+
+export default userRouter;
