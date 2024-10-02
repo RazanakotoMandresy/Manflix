7ff4b69013365a6f07821839e91914162aff4b37
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-02 10:47:09 +0300

    user model

--- Animax_back/src/models/User.ts
+++ Animax_back/src/models/User.ts
@@ -1,9 +1,30 @@
-import { Schema } from "mongoose";
-
-const autSchema = new Schema({
-  UserName: {
-    type: String,
-    required: true,
-    unique: [true, "name should be unique"],
+import { model, Schema } from "mongoose";
+interface IUser {
+  name: string;
+  email: string;
+  passwords: string;
+  videos: Array<string>;
+}
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match:
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      unique: true,
+    },
+    passwords: {
+      type: String,
+      required: true,
+    },
+    videos: Array,
   },
-});
+  { timestamps: true }
+);
+export default model("User", userSchema);
