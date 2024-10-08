a68afd33696ff07c29b8ab5658adf171a54e19a8
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-08 16:27:42 +0300

    bad naming of the portname

--- Animax_back/src/index.ts
+++ Animax_back/src/index.ts
@@ -2,13 +2,14 @@ import "dotenv/config";
 import express from "express";
 import { connectDb } from "./DB/connectDB";
 import userRoutes from "./routes/usersRoutes";
-const PORT = process.env.LOCALPORT;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 app.use("/api/v1/", userRoutes);
+
 const start = async () => {
-  console.log(`trying to start the server}`);
+  console.log(`trying to start the server `);
   try {
     await connectDb(process.env.DATABASE!);
     app.listen(PORT, () => {
