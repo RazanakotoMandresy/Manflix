c4fd9ef378a4eed37e9a881eb855d8170c355b77
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-08 16:34:31 +0300

    forgot to try and catch on login

--- Animax_back/src/controllers/users/authControllers.ts
+++ Animax_back/src/controllers/users/authControllers.ts
@@ -28,30 +28,34 @@ export const register = async (req: Request, res: Response) => {
   }
 };
 export const login = async (req: Request, res: Response) => {
-  const { passwords, email } = req.body;
-  if (!passwords || !email) {
-    res.status(400).json({ res: "email and passwords are required" });
-    return;
+  try {
+    const { passwords, email } = req.body;
+    if (!passwords || !email) {
+      res.status(400).json({ res: "email and passwords are required" });
+      return;
+    }
+    // check passwords
+    const user = await User.findOne({ email });
+    if (!user) {
+      res
+        .status(400)
+        .json({ res: "please verify you have enter the correct email" });
+      return;
+    }
+    const correctPsswd = bcrypt.compareSync(passwords, user?.passwords);
+    if (!correctPsswd) {
+      res.status(401).json({ err: "incrorrect passwords" });
+      return;
+    }
+    const token = signToken(user);
+    if (token == "") {
+      res.status(500).json({ err: "an error occured during token's creation" });
+      return;
+    }
+    res.status(200).json({ res: token });
+  } catch (error) {
+    res.json({ err: error }).status(500);
   }
-  // check passwords
-  const user = await User.findOne({ email });
-  if (!user) {
-    res
-      .status(400)
-      .json({ res: "please verify you have enter the correct email" });
-    return;
-  }
-  const correctPsswd = bcrypt.compareSync(passwords, user?.passwords);
-  if (!correctPsswd) {
-    res.status(401).json({ err: "incrorrect passwords" });
-    return;
-  }
-  const token = signToken(user);
-  if (token == "") {
-    res.status(500).json({ err: "an error occured during token's creation" });
-    return;
-  }
-  res.status(200).json({ res: token });
 };
 const signToken = (auth: any): string => {
   const token = jwt.sign(
@@ -59,6 +63,5 @@ const signToken = (auth: any): string => {
     process.env.JWT_SECRET!,
     { expiresIn: "31d" }
   );
-
   return token;
 };
