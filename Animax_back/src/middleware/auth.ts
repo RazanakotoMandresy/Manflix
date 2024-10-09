3649f0c22546771cc610944b994f7133cd17258d
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-09 08:24:24 +0300

    adding the verify token

--- Animax_back/src/middleware/auth.ts
+++ Animax_back/src/middleware/auth.ts
@@ -1,5 +1,9 @@
-import jwt from "jsonwebtoken";
+import type { NextFunction, Request, Response } from "express";
+import jwt, { verify, type JwtPayload } from "jsonwebtoken";
 
+export interface CustomRequest extends Request {
+  token: string | JwtPayload;
+}
 export const signToken = (auth: any): string => {
   const token = jwt.sign(
     { userId: auth._id, userName: auth.name },
@@ -7,4 +11,22 @@ export const signToken = (auth: any): string => {
     { expiresIn: "31d" }
   );
   return token;
+};
+export const verifyAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeaders = req.headers.authorization;
+  if (!authHeaders || !authHeaders.startsWith("Bearer ")) {
+    return res.status(401).json({ msg: "invalid auth headers" });
+  }
+  const token = authHeaders.split(" ")[1];
+  try {
+    const decode = verify(token, process.env.JWT_SECRET!);
+    (req as CustomRequest).token = decode;
+    next();
+  } catch (error) {
+    res.status(401).json(error);
+  }
 };
